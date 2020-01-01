cbfa27051032204aa5b228b259542101f441ee65
ikemanep1 <ike.esquivelpilloud@gmail.com>
2020-01-01 01:25:52 -0800

    add definitions functionality to the back end

---
+++ lib/definition.rb
@@ -0,0 +1,45 @@
+class Definition
+  attr_reader :id
+  attr_accessor :name, :word_id
+
+  @@definitions = {}
+  @@total_rows = 0
+
+  def initialize(name, word_id, id)
+    @name = name
+    @word_id = word_id
+    @id = id || @@total_rows += 1
+  end
+
+  def ==(definition_to_compare)
+    (self.name() == definition_to_compare.name()) && (self.word_id() == definition_to_compare.word_id())
+  end
+
+  def save
+    @@definitions[self.id] = Definition.new(self.name, self.word_id, self.id)
+  end
+
+  def self.find(id)
+    self.name = name
+    self.word_id = word_id
+    @definitions[self.id] = Word.new(self.name, self.word_id, self.id)
+  end
+
+  def self.clear
+    @@definitions = {}
+  end
+
+  def self.find_by_word(wor_id)
+    definitions = []
+    @@definitions.values.each do |definition|
+      if definition.word_id == word_id
+        definitions.push(definition)
+      end
+    end
+    definitions
+  end
+
+  def words
+    Word.find(self.word_id)
+  end
+end
