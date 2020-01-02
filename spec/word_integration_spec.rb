8f154ac43f0047c2b59de2e8aa892912444e4942
ikemanep1 <ike.esquivelpilloud@gmail.com>
2020-01-01 17:43:27 -0800

    fix integration specs so that they pass

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -7,21 +7,21 @@ describe('create a word path', {:type => :feature}) do
   it('creates a word and then goes to the word page') do
     visit('/words')
     save_and_open_page
-    click_on('Add a New word')
+    click_on('Create a New Word!')
     fill_in('word_name', :with => 'Yellow word')
-    click_on('activate')
+    click_on('Create word!')
     expect(page).to have_content('Yellow word')
   end
 end
 
 describe('create a definition path', {:type => :feature}) do
   it('creates a word and then goes to the word page') do
-    word = Word.new("Apple", "a red fruit")
+    word = Word.new("Apple", nil)
     word.save
     visit("/words/#{word.id}")
     save_and_open_page
-    fill_in('definition_post', :with => 'A red fruit')
-    click_on('Add definition')
+    fill_in('definition_name', :with => 'A red fruit')
+    click_on('Add Definition')
     expect(page).to have_content('A red fruit')
   end
 end
